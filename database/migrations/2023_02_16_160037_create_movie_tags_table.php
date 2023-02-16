34e3c2358c7543b4ed023871212a4dd284c528e4
Yari <narducciyarigmail.com>
2023-02-16 17:02:13 +0100

    completamento

--- database/migrations/2023_02_16_160037_create_movie_tags_table.php
+++ database/migrations/2023_02_16_160037_create_movie_tags_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('movie_tag', function (Blueprint $table) {
-            
-            $table -> id();            
+        Schema::create('movie_tags', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movie_tag');
+        Schema::dropIfExists('movie_tags');
     }
 };
