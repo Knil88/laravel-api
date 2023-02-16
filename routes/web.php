f7c0038b8d1751ef4fa36c6854b0e6a02427edbd
Yari <narducciyarigmail.com>
2023-02-16 16:52:24 +0100

    modifica file

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route :: get('/', [MainController :: class, 'home'])
+    -> name('home');
+Route :: get('/movie/all', [MainController :: class, 'movieAll'])
+    -> name('movie.all');
+
+Route :: get('/movie/create', [MainController :: class, 'movieCreate'])
+    -> name('movie.create');
+Route :: post('/movie/create', [MainController :: class, 'movieStore'])
+    -> name('movie.store');
+
+Route :: get('/movie/edit/{movie}', [MainController :: class, 'movieEdit'])
+    -> name('movie.edit');
+Route :: post('/movie/edit/{movie}', [MainController :: class, 'movieUpdate'])
+    -> name('movie.update');
+
+Route :: get('/movie/delete/{movie}', [MainController :: class, 'movieDelete'])
+    -> name('movie.delete');
+
+
+
+
+use App\Http\Controllers\ApiController;
+
+Route :: get('/api/v1/movie/all', [ApiController :: class, 'movieAll']);
